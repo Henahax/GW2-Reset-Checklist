12f169167ae97ca54c1e455937fbb3978d11f56a
Henahax <mail@henahax.de>
2024-01-25 23:00:50 +0100

    x

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,5 @@
+import type { Item } from "./types";
+
 function getUTCTimeForStartOfNextDay() {
   const now = new Date();
   const tomorrow = new Date(now);
@@ -20,4 +22,34 @@ function getUTCTimeForStartOfNextWeek() {
     nextMonday.setUTCDate(nextMonday.getUTCDate() + 7);
   }
   return nextMonday;
+}
+
+export function getCookieValue(cookieName: string) {
+  const cookies = document.cookie.split(";");
+
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split("=");
+
+    if (name === cookieName) {
+      return value === "true" ? true : value === "false" ? false : null;
+    }
+  }
+  return null;
+}
+
+export function setCookie(event: Event, item: Item, type: string) {
+  let time = new Date();
+  //In 10 years for settings
+  let expires = time.getTime() + 1000 * 60 * 60 * 24 * 365 * 10;
+  time.setTime(expires);
+
+  if (item.interval === "daily") {
+    time = getUTCTimeForStartOfNextDay();
+  }
+  if (item.interval === "weekly") {
+    time = getUTCTimeForStartOfNextWeek();
+  }
+  document.cookie = `${item.id + type}=${
+    event.target.checked
+  }; expires=${time.toUTCString()}`;
 }
--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -1,56 +1,88 @@
 <script lang="ts">
   import EventTimer from "./EventTimer.svelte";
   import type { Item } from "../types";
+  import { getCookieValue } from "../functions";
+  import { setCookie } from "../functions";
   export let item: Item;
+
+  function getDisplayed() {
+    let displayed = getCookieValue(item.id + ".display");
+    if (displayed === null) {
+      if (item.default === true) {
+        return true;
+      }
+      return false;
+    }
+    return displayed;
+  }
+
+  function getChecked() {
+    let checked = getCookieValue(item.id + ".checked");
+    if (checked === null) {
+      return false;
+    }
+    return checked;
+  }
+
+  function setChecked(event) {
+    setCookie(event, item, ".checked");
+  }
 </script>
 
-<li class="inline-flex py-1 justify-between gap-2">
-  <label class="inline-flex items-center gap-4 w-full">
-    <input type="checkbox" class="w-6 h-6 shrink-0" />
-    <img src={item.icon} alt={item.name} class="icon w-8 h-8" />
-    <div>
-      <h4 class="text-sm">{item.name}</h4>
-      <h5 class="text-xs text-neutral-400">{item.info}</h5>
-    </div>
-  </label>
-  <div class="info text-right text-xs text-neutral-400 self-center">
-    <div class="inline-flex gap-1.5">
-      <a href={item.link}>
-        <i class="fa-regular fa-circle-question" />
-      </a>
-      {#if item.interval == "daily"}
-        <div class="tooltip-container">
-          <i
-            class="fa-regular fa-clock self-center"
-            aria-describedby={item.id + "Tooltip"}
-          >
-            <div
-              id={item.id + "Tooltip"}
-              class="tooltip whitespace-nowrap absolute z-10 bg-black text-white rounded-full px-2 py-1 border border-neutral-600 opacity-80 -ml-14"
+{#if getDisplayed()}
+  <li class="inline-flex py-1 justify-between gap-2">
+    <label class="inline-flex items-center gap-4 w-full">
+      <input
+        type="checkbox"
+        class="w-6 h-6 shrink-0"
+        checked={getChecked()}
+        on:click={setChecked}
+      />
+      <img src={item.icon} alt={item.name} class="icon w-8 h-8" />
+      <div>
+        <h4 class="text-sm">{item.name}</h4>
+        <h5 class="text-xs text-neutral-400">{item.info}</h5>
+      </div>
+    </label>
+    <div class="info text-right text-xs text-neutral-400 self-center">
+      <div class="inline-flex gap-1.5">
+        <a href={item.link}>
+          <i class="fa-regular fa-circle-question" />
+        </a>
+        {#if item.interval == "daily"}
+          <div class="tooltip-container">
+            <i
+              class="fa-regular fa-clock self-center"
+              aria-describedby={item.id + "Tooltip"}
             >
-              resets daily
-            </div>
-          </i>
-        </div>
-      {/if}
-      {#if item.interval == "weekly"}
-        <div class="tooltip-container">
-          <i class="fa-regular fa-calendar">
-            <div
-              id={item.id + "Tooltip"}
-              class="tooltip whitespace-nowrap absolute z-10 bg-red-500 text-white rounded-full px-2 py-1 border border-neutral-600 opacity-80 -ml-14"
-            >
-              resets weekly
-            </div>
-          </i>
-        </div>
+              <div
+                id={item.id + "Tooltip"}
+                class="tooltip whitespace-nowrap absolute z-10 bg-black text-white rounded-full px-2 py-1 border border-neutral-600 opacity-80 -ml-14"
+              >
+                resets daily
+              </div>
+            </i>
+          </div>
+        {/if}
+        {#if item.interval == "weekly"}
+          <div class="tooltip-container">
+            <i class="fa-regular fa-calendar">
+              <div
+                id={item.id + "Tooltip"}
+                class="tooltip whitespace-nowrap absolute z-10 bg-red-500 text-white rounded-full px-2 py-1 border border-neutral-600 opacity-80 -ml-14"
+              >
+                resets weekly
+              </div>
+            </i>
+          </div>
+        {/if}
+      </div>
+      {#if item.timer}
+        <EventTimer timer={item.timer} />
       {/if}
     </div>
-    {#if item.timer}
-      <EventTimer timer={item.timer} />
-    {/if}
-  </div>
-</li>
+  </li>
+{/if}
 
 <style>
   li:has(input:checked) {
