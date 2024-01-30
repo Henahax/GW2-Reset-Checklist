e4d921642cec893eab278149896d1268a924f8b7
Henahax <mail@henahax.de>
2024-01-31 00:58:08 +0100

    rc

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -21,7 +21,8 @@
   </button>
 
   <dialog id="settings" class="border rounded-xl border-neutral-500">
-    <div id="top"
+    <div
+      id="top"
       class="inline-flex text-left font-bold text-lg justify-between w-full px-3 py-1"
     >
       Displayed items
@@ -29,13 +30,13 @@
         <i class="fa-solid fa-xmark self-center" />
       </button>
     </div>
-
-    <div id="settingsContainer" class="px-2">
-      {#each data as category}
-        <SettingsCategory {category} />
-      {/each}
+    <div id="settingsContainer" class="flex flex-col">
+      <div id="settingItemsContainer" class="px-2 overflow-y-auto overflow-x-hidden">
+        {#each data as category}
+          <SettingsCategory {category} />
+        {/each}
+      </div>
     </div>
-
     <div id="bottom" class="inline-flex w-full">
       <button
         class="rounded-full bg-neutral-800 py-1 px-6 mx-auto my-4 border border-neutral-500 hover:scale-110"
@@ -47,10 +48,10 @@
 
 <style>
   #settingsContainer {
-    columns: 3 16rem;
+    height: calc(100dvh - 10rem);
   }
-
-  dialog:modal {
+  #settingItemsContainer {
+    columns: 3 16rem;
   }
 
   dialog::backdrop {
